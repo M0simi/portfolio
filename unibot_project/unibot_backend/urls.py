acba3562566d65398e6c08831f23d6c17926ff9b
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-11-06 14:16:34 +0300

    Update urls.py

--- unibot_project/unibot_backend/urls.py
+++ unibot_project/unibot_backend/urls.py
@@ -2,13 +2,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),
+    # Django admin
+    path("admin/", admin.site.urls),
+
+    # API 
+    path("api/", include("core.urls")),
+
+    # dashboard
     path("dashboard/", include("custom_admin.urls")),
+
+    # test
+    path("healthz/", lambda r: JsonResponse({"ok": True}, status=200)),
+    path("api/ping/", lambda r: JsonResponse({"pong": True}, status=200)),
 ]
 
-# قدّم ملفات MEDIA حتى على Render
+# media
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
