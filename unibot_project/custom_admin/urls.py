cf5f93b156a09f6d30a24b8fb08b51e725b8d0f8
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-11-09 12:51:21 +0300

    Update urls.py

--- unibot_project/custom_admin/urls.py
+++ unibot_project/custom_admin/urls.py
@@ -5,32 +5,4 @@ app_name = "custom_admin"
 
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
-
-    path("events/", views.events_list, name="events_list"),
-    path("events/add/", views.event_add, name="event_add"),
-    path("events/<int:pk>/edit/", views.event_edit, name="event_edit"),
-    path("events/<int:pk>/delete/", views.event_delete, name="event_delete"),
-
-    path("categories/", views.categories_list, name="categories_list"),
-    path("categories/add/", views.category_add, name="category_add"),
-    path("categories/<int:pk>/edit/", views.category_edit, name="category_edit"),
-    path("categories/<int:pk>/delete/", views.category_delete, name="category_delete"),
-
-    path("faqs/", views.faqs_list, name="faqs_list"),
-    path("faqs/add/", views.faq_add, name="faq_add"),
-    path("faqs/<int:pk>/edit/", views.faq_edit, name="faq_edit"),
-    path("faqs/<int:pk>/delete/", views.faq_delete, name="faq_delete"),
-
-    path("favorites/", views.favorites_list, name="favorites_list"),
-    path("favorites/<int:pk>/delete/", views.favorite_delete, name="favorite_delete"),
-
-    path("feedback/", views.feedback_list, name="feedback_list"),
-    path("feedback/<int:pk>/delete/", views.feedback_delete, name="feedback_delete"),
-
-    path("users/", views.users_list, name="users_list"),
-
-    path("knowledge/", views.knowledgebases_list, name="knowledgebases_list"),
-    path("knowledge/add/", views.kb_add, name="kb_add"),
-    path("knowledge/<int:pk>/delete/", views.kb_delete, name="kb_delete"),
-    path("fix/kb/reset/", views.kb_reset, name="kb_reset"),
 ]
