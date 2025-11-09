c2e3875b5d33e0dca9ee97308f52a4c6824dee1b
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-11-09 18:04:47 +0300

    Update urls.py

--- unibot_project/unibot_backend/urls.py
+++ unibot_project/unibot_backend/urls.py
@@ -1,20 +1,6 @@
-from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
-    # لوحة التحكم المخصّصة (تظل الصفحة الرئيسية على الداشبورد)
-    path("", include(("custom_admin.urls", "custom_admin"), namespace="custom_admin")),
-
-    # لوحة Django Admin الأصلية (نبقيها)
+    path("api/", include("core.urls")),   # لا تغيّر المسار
     path("admin/", admin.site.urls),
-
-    # REST API
-    path("api/", include(("core.urls", "core"), namespace="core")),
+    path("", include(("custom_admin.urls","custom_admin"), namespace="custom_admin")),
 ]
-
-# ميديا أثناء التطوير/أو لو تحتاج تخدمها مباشرة
-# (في الإنتاج الأفضل يكون عبر Cloudinary/Storage أو Nginx، لكن هذا السطر ما يضر)
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
