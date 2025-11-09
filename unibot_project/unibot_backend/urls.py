b97064b425e97b64ad16f8b0947cf9790436ae0a
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-11-09 16:07:32 +0300

    Update urls.py

--- unibot_project/unibot_backend/urls.py
+++ unibot_project/unibot_backend/urls.py
@@ -1,13 +1,10 @@
-# unibot_backend/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # لوحتك المخصّصة تكون على الجذر وعلى /dashboard/ أيضاً
+    
     path("", include(("custom_admin.urls", "custom_admin"), namespace="custom_admin")),
 
-    # خلي صفحة أدمن دجانغو موجودة (الميدلوير بيحوّل /admin إلى لوحتك)
+   
     path("admin/", admin.site.urls),
-
-    
 ]
