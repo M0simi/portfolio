92982eccd126d0e911256f99d47ae0fad3c2d724
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-11-09 16:46:09 +0300

    Update urls.py

--- unibot_project/unibot_backend/urls.py
+++ unibot_project/unibot_backend/urls.py
@@ -4,15 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-   
-    path("admin/", admin.site.urls),
-
-    
-    path("api/", include(("core.urls", "core"), namespace="core")),
-
-   
-    path("", include(("custom_admin.urls", "custom_admin"), namespace="custom_admin")),
+    path("admin/", admin.site.urls),                                    
+    path("api/", include(("core.urls", "core"), namespace="core")),    
+    path("", include(("custom_admin.urls", "custom_admin"), namespace="custom_admin")),  # الداشبورد
 ]
 
-# media
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
